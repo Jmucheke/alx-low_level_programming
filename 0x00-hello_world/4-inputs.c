795e7a45a07619a7e18d0288e6ea6acad8d0830e
jmucheke <jmucheke274@gmail.com>
2025-05-04 19:40:30 +0300

    written a c program to print a string using puts function

---
+++ 0x00-hello_world/4-inputs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * print a string
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
