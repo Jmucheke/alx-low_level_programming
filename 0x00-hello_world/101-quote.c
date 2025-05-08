e1289674caaa966da2908092a6e26a18bbb309df
jmucheke <jmucheke274@gmail.com>
2025-05-08 07:18:31 +0300

    removed white spaced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,15 @@
 /**
  * main - printing to standard error
  *
+ * Description: Prints a message to stderr
+ *
  * Return: Always 1 (per instructions)
  */
 
 int main(void)
 {
 	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
+
 	write(STDERR_FILENO, msg, 59);
 	return (1);
 }
